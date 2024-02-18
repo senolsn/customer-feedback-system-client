113f5d974d17c846400b5c1addb3d95ec4668965
Senol <senoltr@yandex.com>
2024-02-18 17:57:33 +0300

    admin dashboard complaint işlemleri tamamlandı.

--- src/app/components/complaints/complaints.component.ts
+++ src/app/components/complaints/complaints.component.ts
@@ -1,8 +1,10 @@
 // complaints.component.ts
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { jwtDecode } from 'jwt-decode';
+import { ToastrService } from 'ngx-toastr';
 import { Complaint } from 'src/app/models/complaint';
 import { Employee } from 'src/app/models/employee';
+import { UpdateComplaintModelForEmployee } from 'src/app/models/updateComplaintModelForEmployee';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { ComplaintsService } from 'src/app/services/complaints/complaints.service';
 import { EmployeeService } from 'src/app/services/employee/employee.service';
@@ -15,10 +17,14 @@ import { EmployeeService } from 'src/app/services/employee/employee.service';
 export class ComplaintsComponent implements OnInit {
   unResolvedComplaints: Complaint[] = [];
   employee: Employee;
+  updateComplaintsModel:UpdateComplaintModelForEmployee;
+  employeeNoteValue: string = '';
+
 
   constructor(
     private complaintService: ComplaintsService,
-    private employeeService: EmployeeService
+    private employeeService: EmployeeService,
+    private toastr:ToastrService
   ) {}
 
   ngOnInit(): void {
@@ -45,5 +51,60 @@ export class ComplaintsComponent implements OnInit {
       this.getUnresolvedComplaints(this.employee.id);
       console.log(this.unResolvedComplaints);
     });
+  }
+
+  updateEmployeeNoteForComplaints(complaint:Complaint){
+    let employeeNoteValue = this.employeeNoteValue.trim(); // Trim kullanarak başındaki ve sonundaki boşlukları temizleyin
+    if (!employeeNoteValue) {
+        employeeNoteValue = "Şikayetiniz tarafımızca çözülmüştür."; // Varsayılan mesaj
+    }
+
+    let updateComplaintsModel:UpdateComplaintModelForEmployee ={
+        complaintId: complaint.complaintId,
+        customerId: complaint.customer.id,
+        employeeId: complaint.employee.id,
+        complaintStatus: 1,
+        description: complaint.description,
+        employeeNote: this.employeeNoteValue,
+        title: complaint.title
+    }
+
+    this.complaintService.updateForEmployee(updateComplaintsModel).subscribe(
+      response => {
+      this.getEmployeeAndComplaints(this.employee.email)
+      this.toastr.success("Not güncellendi !")
+      
+      },
+      responseError => {
+          this.toastr.error(responseError.error.message)
+          // Hata durumunda yapılacak işlemler burada yapılabilir
+      }
+  );
+    
+  }
+
+  markComplaintAsResolved(complaint: Complaint): void {
+    // Complaint türünden bir nesneyi UpdateComplaintModelForEmployee türüne dönüştür
+    let updateComplaintsModel: UpdateComplaintModelForEmployee = {
+        complaintId: complaint.complaintId,
+        customerId: complaint.customer.id,
+        employeeId: complaint.employee.id,
+        complaintStatus: 2,
+        description: complaint.description,
+        employeeNote: "Şikayetiniz tarafımızca çözülmüştür!",
+        title: complaint.title
+    };
+
+    // updateForEmployee metodunu çağır
+    this.complaintService.updateForEmployee(updateComplaintsModel).subscribe(
+        response => {
+        this.getEmployeeAndComplaints(this.employee.email)
+        this.toastr.success("Şikayet Çözüldü!","Tebrikler")
+        },
+        responseError => {
+            this.toastr.error(responseError.error.message)
+            // Hata durumunda yapılacak işlemler burada yapılabilir
+        }
+    );
   }
 }
